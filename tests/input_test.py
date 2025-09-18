ca9bec4bc09cd605d2f57acace2f3ae612536174
mpryor <mapryor1991@yahoo.com>
2025-09-18 16:46:52 -0600

    chore: fix input_test

--- tests/input_test.py
+++ tests/input_test.py
@@ -1,16 +1,14 @@
 import sys
 import argparse
-from input import InputConsumer
-
+from nless.input import InputConsumer
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Test InputConsumer with stdin.")
     parser.add_argument("filename", nargs="?", help="File to read input from (defaults to stdin)")
     args = parser.parse_args()
     if args.filename:
-        with open(args.filename, "r") as f:
-            ic = InputConsumer(f.fileno(), lambda: True, lambda lines: print(lines))
-            ic.run()
+        ic = InputConsumer(args.filename, None, lambda: True, lambda lines: print(lines))
+        ic.run()
     else:
-        ic = InputConsumer(sys.stdin.fileno(), lambda: True, lambda lines: print(lines))
+        ic = InputConsumer(None, sys.stdin.fileno(), lambda: True, lambda lines: print(lines))
         ic.run()
