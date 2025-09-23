ee29a2ee44576d4f0aaf939e3e23a3eae7ad490c
mpryor <mapryor1991@yahoo.com>
2025-09-22 18:42:05 -0600

    feat: vim-style movement

--- nless.py
+++ nless.py
@@ -14,37 +14,82 @@ class NlessApp(App):
 
     BINDINGS = [
         ("q", "quit", "Quit"),
+        ("G", "scroll_to_bottom", "Scroll to Bottom"),
+        ("g", "scroll_to_top", "Scroll to Top"),
+        ("d", "page_down", "Page Down"),
+        ("u", "page_up", "Page up"),
         ("up", "cursor_up", "Up"),
+        ("k", "cursor_up", "Up"),
         ("down", "cursor_down", "Down"),
+        ("j", "cursor_down", "Down"),
+        ("l", "cursor_right", "Right"),
+        ("h", "cursor_left", "Left"),
+        ("f", "filter", "Filter"),
     ]
 
     def __init__(self):
         super().__init__()
         self.mounted = False
+        self.first_row_parsed = False
         self.raw_rows = []
 
     def compose(self) -> ComposeResult:
         """Create and yield the DataTable widget."""
         table = DataTable()
-        table.add_columns("Log")
         yield table
         yield Footer()
+
+    def action_filter(self) -> None:
+        """Filter rows based on user input."""
+
 
     def action_cursor_up(self) -> None:
         """Move cursor up."""
         self.query_one(DataTable).action_cursor_up()
 
-    def on_mount(self) -> None:
-        self.mounted = True
-
     def action_cursor_down(self) -> None:
         """Move cursor down."""
         self.query_one(DataTable).action_cursor_down()
 
+    def action_cursor_left(self) -> None:
+        """Move cursor left."""
+        self.query_one(DataTable).action_cursor_left()
+
+    def action_cursor_right(self) -> None:
+        """Move cursor left."""
+        self.query_one(DataTable).action_cursor_right()
+
+    def action_scroll_to_bottom(self) -> None:
+        """Scroll to top."""
+        self.query_one(DataTable).action_scroll_bottom()
+
+    def action_scroll_to_top(self) -> None:
+        """Scroll to top."""
+        self.query_one(DataTable).action_scroll_top()
+
+    def action_page_up(self) -> None:
+        """Scroll to top."""
+        data_table = self.query_one(DataTable)
+        data_table.action_page_up()
+
+    def action_page_down(self) -> None:
+        """Scroll to top."""
+        data_table = self.query_one(DataTable)
+        data_table.action_page_down()
+
+    def on_mount(self) -> None:
+        self.mounted = True
+
     def add_log(self, log_line: str) -> None:
         if self.mounted:
             data_table = self.query_one(DataTable)
-            data_table.add_row(log_line)
+
+            if not self.first_row_parsed:
+                columns = [f"col{i}" for i in range(1, len(log_line.split(",")) + 1)]
+                data_table.add_columns(*columns)
+                self.first_row_parsed = True
+
+            data_table.add_row(*log_line.split(","))
             self.raw_rows.append(log_line)
 
 
